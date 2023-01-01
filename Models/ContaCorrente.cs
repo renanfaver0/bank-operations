d7b7a18621758ad5bd5aa1c02badd92e330967f8
renanfaver0 <renaan.favero@gmail.com>
2022-12-31 22:34:51 -0300

    Programa simples simulando um banco

---
+++ Models/ContaCorrente.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaveroBank.Models
+{
+    // Sentenciando a classe ContaCorrente;
+    public class ContaCorrente
+    {
+        // Declanado as variáveis;
+        private decimal depositoConta = 0;
+        private decimal saqueConta = 0;
+        private decimal calculaValor = 0;
+        private decimal novoValorDepositado = 0;
+
+        public ContaCorrente(decimal depositoConta, decimal saqueConta, decimal calculaValor, decimal novoValorDepositado)
+        {
+            this.depositoConta = depositoConta;
+            this.saqueConta = saqueConta;
+            this.calculaValor = calculaValor;
+            this.novoValorDepositado = novoValorDepositado;
+        }
+
+        // Adicionando o método DepositarValor, para o depósito de valores no programa;
+        public void DepositarValor()
+        {
+            Console.WriteLine("Digite a quantidade de valor que você deseja depositar:");
+            depositoConta = Convert.ToDecimal(Console.ReadLine());
+
+        // Fazendo com que o programa faça a soma de novos valores adicionados junto aos valores que já foi adicionado anteriormente;
+            novoValorDepositado = (calculaValor += depositoConta);
+
+        // Validando para que o programa não aceite valores 0 ou menores para depositos;
+            if(depositoConta <= 0)
+            {
+                Console.WriteLine("Valor não permitido para Depósito!................");
+                Console.ReadLine();
+            }
+            else 
+            {
+                Console.WriteLine($"O valor de: {depositoConta} reais foi depositado em sua conta com sucesso!................");
+            }
+        }
+
+        // Adicionando o método SacarValor, para realizar saques no sistema;
+        public void SacarValor() 
+        {
+            Console.WriteLine("Digite um valor para saque: ");
+            saqueConta = Convert.ToDecimal(Console.ReadLine());
+
+        // Validando para que o usuário não consiga sacar um valor que não tenha disponível em sua conta, valida também para o usuário não sacar uma valor menor ou igual a 0;
+            if (calculaValor < saqueConta || saqueConta <= 0) 
+            {
+                Console.Write("Você não pode sacar um valor maior que o valor que possuí em sua conta, ou um valor menor ou igual a 0................!");
+                Console.ReadLine();
+            }
+            
+        // Validando o valor da conta toda vez que retira, ou seja, cada vez que o usuário fazer um saque vai diminuindo o valor na conta.
+            else
+            {
+                calculaValor = (depositoConta -= saqueConta);
+                Console.WriteLine($"Saque no valor de {saqueConta} reais realizado!................");
+            }
+
+        }
+        
+        // Adicionando o método ExtratoValor, para que o usuário consiga consultar o seu extrato bancário no sistema.
+        public void ExtratoValor()
+        {   
+            Console.WriteLine($"O valor total do extrato da sua conta é de: {calculaValor} reais................");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using FaveroBank.Models;
+
+// Adicionando Encoding para permitr acentuação no código;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// Declarando as variáveis utilizadas no código;
+decimal depositoConta = 0;
+decimal saqueConta = 0;
+decimal calculaValor = 0;
+decimal novoValorDepositado = 0;
+
+// Adicionando o número para finalizar o programa;
+string opcoesMenu = string.Empty;
+
+// Opções do menu do switch case;
+bool menuBanco = true;
+
+// Sentenciando a classe ContaCorrente;
+ContaCorrente cc = new ContaCorrente(depositoConta, saqueConta, calculaValor, novoValorDepositado);
+
+// Menu do programa
+while (menuBanco)
+{
+    Console.Clear();
+    Console.WriteLine("::Bem vindo ao Favero Bank, o seu banco eletrônico::");
+    Console.WriteLine("Digite a opção desejada: ");
+    Console.WriteLine("1 - Depositar valores");
+    Console.WriteLine("2 - Sacar valores");
+    Console.WriteLine("3 - Extrato bancário");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            cc.DepositarValor();
+            break;
+
+        case "2":
+            cc.SacarValor();
+            break;
+
+        case "3":
+            cc.ExtratoValor();
+            break;
+
+        case "4":
+            menuBanco = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
+Console.WriteLine("O programa se encerrou");
+
+
+
+
+
+
